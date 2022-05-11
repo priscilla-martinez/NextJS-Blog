6d4704378919293c125736314f02e1b362bb9f67
Priscilla Martinez <priscillamartinez@PriscillasMBP2.attlocal.net>
2022-05-11 12:03:02 -0700

    implemented fetching external data and pre-render the index page with the data

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,20 @@
 import Head from 'next/head';
-import Layout, { siteTitle } from '../components/layout';
-import utilStyles from '../styles/utils.module.css';
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
+import { getSortedPostsData } from '../lib/posts'
 
-export default function Home() {
+
+export async function getStaticProps() {
+    const allPostsData = getSortedPostsData()
+    return {
+        // By returning 'allPostsData' inside 'props' the blog posts will be passed to 'Home' below
+        props: {
+            allPostsData,
+        },
+    }
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
         <Head>
@@ -11,10 +23,26 @@ export default function Home() {
         <section className={utilStyles.headingMd}>
             <p>[Hello! I'm learning Next.JS because we are rebuilding the FMC Website with it.]</p>
             <p>
-                (This is a sample website - you’ll be building a site like this on{' '}
+                (This is a sample website - youll be building a site like this on{' '}
                 <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
             </p>
         </section>
+
+        {/* Displaying blog posts */}
+        <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+            <h2 className={utilStyles.headingLg}>Blog</h2>
+            <ul className={utilStyles.list}>
+                {allPostsData.map(({ id, date, title }) => (
+                    <li className={utilStyles.listItem} key={id}>
+                        {title}
+                        <br/>
+                        {id}
+                        <br/>
+                        {date}
+                    </li>
+                ))}
+            </ul>   
+        </section>
     </Layout>
-  );
+  )
 }
